1134eaf074a357f54422b520e2b9915d4077d861
mlacadie <lacadiem14@gmail.com>
2023-10-08 16:33:11 -0400

    Upload

--- src/App.js
+++ src/App.js
@@ -4,19 +4,17 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
-  const [URL, setURL] = useState('test.com');
+  const [URL, setURL] = useState('Placeholder');
 
   return (
-    <div className="App">
-      <input
-              type="text" 
-              id="URL-Field"
-              onChange={(e) => setURL(e.target.value)}
-              value={URL}
-            />
-      <div 
-        className='File-Input'
-      />
+    <div className="container mt-4">
+      <div className="App">
+        <span className="form-control" id="URL-Field">{URL}</span>
+        <div className="File-Input">
+          <input className='Upload-Button' title='Upload File' type='file'/>
+          <span className="Upload-Text">Upload or Drag Files</span>
+        </div>
+      </div>
     </div>
   );
 }
