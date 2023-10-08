4b9683b1dd521dce0c3a96739448f1e38569642a
MarekSGarbaczonek <66498850+MarekSGarbaczonek@users.noreply.github.com>
2023-10-08 14:56:13 -0400

    URL Entry

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [URL, setURL] = useState('test.com');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Container'>
+        <input
+                type="text" 
+                id="URL-Field"
+                onChange={(e) => setURL(e.target.value)}
+                value={URL}
+              />
+      </div>
     </div>
   );
 }
