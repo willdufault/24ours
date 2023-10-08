22d363fe318f3d3c57098e394f0b8cc017824ec8
MarekSGarbaczonek <66498850+MarekSGarbaczonek@users.noreply.github.com>
2023-10-08 19:13:20 -0400

    Upload Frontend

--- src/App.js
+++ src/App.js
@@ -2,22 +2,40 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { FileUploader } from "react-drag-drop-files";
+import Dropzone from 'react-dropzone'
+
 
 function App() {
   const [URL, setURL] = useState('File URL');
+  const [file, setFile] = useState(null);
+  const [dropText, setDropText] = useState('Choose A File Or Drag It Here');
 
-  function getFile(e){
-    const fileInput = e.target;
-    setURL(fileInput.files[0].name);
-  }
+  const fileTypes = ["JPG", "PNG", "GIF"];
+
+  const getFile = (file) => {
+    console.log(file[0]);
+    setFile(file[0]);
+    setDropText(file[0].name);
+  };
 
   return (
     <div className="container mt-4">
       <div className="App">
-        <span className="form-control" id="URL-Field">{URL}</span>
-        <div className="File-Input">
-          <input className='Upload-Button' id='File' title='Upload File' type='file' onChange={getFile}/>
-          <span className="Upload-Text">Upload or Drag File</span>
+        <div className="Container">
+          <span className="form-control" id="URL-Field">{URL}</span>
+          <div className="File-Input">
+            <Dropzone onDrop={getFile}>
+              {({getRootProps, getInputProps}) => (
+                <section className="Upload-Area">
+                  <div className="Drop-Area" {...getRootProps()}>
+                    <input {...getInputProps()} />
+                    <span className="Drop-Text" >{dropText}</span>
+                  </div>
+                </section>
+              )}
+            </Dropzone>
+          </div>
         </div>
       </div>
     </div>
