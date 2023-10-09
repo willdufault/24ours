b3411d3038a11ddb7be4c30fee2c8e070c6d1a3b
MarekSGarbaczonek <66498850+MarekSGarbaczonek@users.noreply.github.com>
2023-10-09 13:43:16 -0400

    No Working Contd

---
+++ frontend/src/App.js
@@ -0,0 +1,69 @@
+import './App.css';
+import React, { useState, useEffect } from 'react';
+//import axios from 'axios';
+import Dropzone from 'react-dropzone'
+
+
+function App() {
+  const [URL, setURL] = useState('File URL');
+  const [file, setFile] = useState(null);
+  const [dropText, setDropText] = useState('Choose A File Or Drag It Here');
+
+  const getFile = (acceptedFiles) => {
+    const inFile = acceptedFiles[0];
+    // console.log(file.size);
+    if (inFile.size > 5300000) {
+      setDropText('File is too big! The limit is 5MB');
+    } else {
+      setFile(inFile);
+      setDropText(inFile.name);
+    }
+  };
+
+  const uploadFile = async () => {
+    
+    const requestData = {
+      fileName: file.name,
+      fileBody: file
+    }
+
+    const response = await fetch( '/uploadfile', {
+      method:'POST',
+      body: JSON.stringify(requestData),
+      headers: { 'Content-Type': 'application/json' }
+    })
+
+    const resData = await response.json();
+    console.log(resData);
+    // await axios.post('/uploadfile', requestData);
+    // const response = await axios.get('/appdata');
+    // console.log(response.data);
+  }
+
+  return (
+    <div className="container mt-4">
+      <div className="App">
+        <div className="Container">
+          <div className="Top-Form">
+            <span className="form-control" id="URL-Field">{URL}</span>
+            <button className="btn btn-primary" onClick={(e) => uploadFile()}>Submit</button>
+          </div>
+          <div className="File-Input">
+            <Dropzone onDrop={getFile}>
+              {({getRootProps, getInputProps}) => (
+                <section className="Upload-Area">
+                  <div className="Drop-Area" {...getRootProps()}>
+                    <input {...getInputProps()} />
+                    <span className="Drop-Text" >{dropText}</span>
+                  </div>
+                </section>
+              )}
+            </Dropzone>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- server.js
+++ server.js
@@ -1,42 +1,56 @@
-// Configure Express.js.
+const path = require('path');
 let express = require('express');
 let app = express();
-let port = 5000;
-app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
-});
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 // Configure axios.
-let axios = require('axios');
-let instance = axios.create({
-	// TODO: NEED A WAY TO HIDE THIS IN GITHUB.
-	baseURL: 'https://y913nvzd14.execute-api.us-east-1.amazonaws.com/dev'
-});
+// let axios = require('axios');
+// let instance = axios.create({
+// 	// TODO: NEED A WAY TO HIDE THIS IN GITHUB.
+// 	baseURL: 'https://y913nvzd14.execute-api.us-east-1.amazonaws.com/dev'
+// });
 
 // GET and POST requests.
-app.get('/', (request, result) => {
-	instance.post('/uploadfile', {
-		'fileName': 'testing.txt',
-		'fileBody': 'wowziers this is working'
-	})
-	.then(response => {
-		result.send(response.data);
-	})
-	.catch((error) => {
-		result.send({'message': error});
-	});
-});
+// app.get('/', (request, result) => {
+// 	// instance.post('/uploadfile', {
+// 	// 	'fileName': 'testing.txt',
+// 	// 	'fileBody': 'wowziers this is working'
+// 	// })
+// 	// .then(response => {
+// 	// 	result.send(response.data);
+// 	// })
+// 	// .catch((error) => {
+// 	// 	result.send({'message': error});
+// 	// });
+// });
 
 app.post('/uploadfile', async (request, result) => {
-	instance.post('/uploadfile', {
-		'fileName': request.body.fileName,
-		'fileBody': new FileReader().readAsText(request.body.fileBody)
-	})
-	.then(response => {
-		result.status(200).json(response.data);
-		response.end(request.json);
-	})
-	.catch((error) => {
-		result.status(400).json({'message': error});
-	});
+	// instance.post('/uploadfile', {
+	// 	'fileName': request.body.fileName,
+	// 	'fileBody': new FileReader().readAsText(request.body.fileBody)
+	// })
+	// .then(response => {
+	// 	result.status(200).json(response.data);
+	// 	response.end(request.json);
+	// })
+	// .catch((error) => {
+	// 	result.status(400).json({'message': error});
+	// });
+
+	const json = request.body;
+
+  	response.writeHead( 200, { 'Content-Type': 'application/json'})
+
+	console.log( request.json )
+
+	response.end( request.json )
 });
+
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/frontend/build', 'index.html'));
+});
+
+const listener = app.listen( 5000 || 3000 )
