05d36747db3326a94cc14495a759035fe8ba3984
wduf <wduf02@gmail.com>
2023-10-09 17:57:38 -0400

    added .env, cleaned up .gitignore, updated server.js to use pure b64 (working w/ lambda)

--- server.js
+++ server.js
@@ -1,52 +1,68 @@
 const path = require('path');
 let express = require('express');
 let app = express();
+let port = 3000;
+app.use(express.json());
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+});
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
+// Configure environment variables.
+require('dotenv').config()
+
 // Configure axios.
-// let axios = require('axios');
-// let instance = axios.create({
-// 	// TODO: NEED A WAY TO HIDE THIS IN GITHUB.
-// 	baseURL: 'https://y913nvzd14.execute-api.us-east-1.amazonaws.com/dev'
-// });
-
-// GET and POST requests.
-// app.get('/', (request, result) => {
-// 	// instance.post('/uploadfile', {
-// 	// 	'fileName': 'testing.txt',
-// 	// 	'fileBody': 'wowziers this is working'
-// 	// })
-// 	// .then(response => {
-// 	// 	result.send(response.data);
-// 	// })
-// 	// .catch((error) => {
-// 	// 	result.send({'message': error});
-// 	// });
-// });
-
-app.post('/uploadfile', async (request, result) => {
-	// instance.post('/uploadfile', {
-	// 	'fileName': request.body.fileName,
-	// 	'fileBody': new FileReader().readAsText(request.body.fileBody)
-	// })
-	// .then(response => {
-	// 	result.status(200).json(response.data);
-	// 	response.end(request.json);
-	// })
-	// .catch((error) => {
-	// 	result.status(400).json({'message': error});
-	// });
-
-	const json = request.body;
-
-  	response.writeHead( 200, { 'Content-Type': 'application/json'})
-
-	console.log( request.json )
-
-	response.end( request.json )
+let axios = require('axios');
+let instance = axios.create({
+	baseURL: process.env.API_ENDPOINT_URL
 });
 
+// GET and POST requests.
+app.get('/', (request, result) => {
+	result.send('hi from express server!')
+});
+
+app.post('/uploadfile', async (request, result) => {
+	instance.post('/uploadfile', {
+		'fileName': request.body.fileName,
+		'fileBody': request.body.fileBody
+	})
+	.then(response => {
+		result.status(200).json(response.data);
+	})
+	.catch((error) => {
+		result.status(400).json({'message': error});
+	});
+});
+
+// ***********************
+// will: i added the above implementation of '/uploadfile'. i think it should work for what the 
+// node.js server needs to do but pls message me on discord if it's missing something
+// ***********************
+// app.post('/uploadfile', async (request, result) => {
+// 	// instance.post('/uploadfile', {
+// 	// 	'fileName': request.body.fileName,
+// 	// 	'fileBody': new FileReader().readAsText(request.body.fileBody)
+// 	// })
+// 	// .then(response => {
+// 	// 	result.status(200).json(response.data);
+// 	// 	response.end(request.json);
+// 	// })
+// 	// .catch((error) => {
+// 	// 	result.status(400).json({'message': error});
+// 	// });
+
+// 	const json = request.body;
+
+//   	response.writeHead( 200, { 'Content-Type': 'application/json'})
+
+// 	console.log( request.json )
+
+// 	response.end( request.json )
+// });
+
+// TODO: PLEASE ADD COMMENTS TO THIS SECTION
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 
 app.get('*', (req, res) => {
