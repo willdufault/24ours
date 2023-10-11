b6c81c9584357fce40d69b00774f65013a42ca1b
wduf <wduf02@gmail.com>
2023-10-11 14:15:01 -0400

    cleaned up + commented server.js

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
-const path = require('path');
-const bodyParser = require('body-parser');
+// Configure Express.js.
 let express = require('express');
 let app = express();
 let port = 3000;
-app.use(bodyParser.json({limit: '10mb'}));
-// app.use(bodyParser.urlencoded({limit: '10mb', extended: true}));
 app.use(express.json());
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
 });
+
+// Set payload size to 10mb.
+let bodyParser = require('body-parser');
+app.use(bodyParser.json({limit: '10mb'}));
+
+// Serve React app.
+let path = require('path');
+app.use(express.static(path.join(__dirname, '/frontend/build')));
 
 // Configure environment variables.
 require('dotenv').config()
@@ -20,10 +25,12 @@ let instance = axios.create({
 });
 
 // GET and POST requests.
-// app.get('/', (request, result) => {
-// 	result.send('hi from express server!')
-// });
+// Load the React project when the page is loaded.
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '/frontend/build', 'index.html'));
+});
 
+// Forward the payload to API Gateway.
 app.post('/uploadfile', async (request, result) => {
 	instance.post('/uploadfile', {
 		'fileName': request.body.fileName,
@@ -36,38 +43,3 @@ app.post('/uploadfile', async (request, result) => {
 		result.status(400).json({'message': error});
 	});
 });
-
-// ***********************
-// will: i added the above implementation of '/uploadfile'. i think it should work for what the 
-// node.js server needs to do but pls message me on discord if it's missing something
-// ***********************
-// app.post('/uploadfile', async (request, result) => {
-// 	// instance.post('/uploadfile', {
-// 	// 	'fileName': request.body.fileName,
-// 	// 	'fileBody': new FileReader().readAsText(request.body.fileBody)
-// 	// })
-// 	// .then(response => {
-// 	// 	result.status(200).json(response.data);
-// 	// 	response.end(request.json);
-// 	// })
-// 	// .catch((error) => {
-// 	// 	result.status(400).json({'message': error});
-// 	// });
-
-// 	const json = request.body;
-
-//   	response.writeHead( 200, { 'Content-Type': 'application/json'})
-
-// 	console.log( request.json )
-
-// 	response.end( request.json )
-// });
-
-// TODO: PLEASE ADD COMMENTS TO THIS SECTION
-app.use(express.static(path.join(__dirname, '/frontend/build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/frontend/build', 'index.html'));
-});
-
-// const listener = app.listen( 5000 || 3000 )
