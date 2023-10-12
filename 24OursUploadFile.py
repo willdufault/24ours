f42d4727aa6c49e2301841e9868196e6abdd357a
Will Dufault <99445180+willdufault@users.noreply.github.com>
2023-10-12 09:06:11 -0400

    added comments and type hints

--- 24OursUploadFile.py
+++ 24OursUploadFile.py
@@ -1,47 +1,38 @@
 import boto3
-import base64
-import lzstring
+from base64 import b64decode
 
 # Create boto3 clients.
 s3_client = boto3.client('s3')
 
-# The event is a file, body file name file contents
-def lambda_handler(event, context):
-    
-    
-    # This isnt actually in b64 just reusing the var name
-    b64_decode = lzstring.decompress(event["fileBody"])
-    
-    print(b64_decode)
-    
-    # b64_decode = base64.b64decode(event["fileBody"])
-    
+def lambda_handler(event: dict, context: object) -> dict:
+    """
+    Create a file from the given file name and body, then upload it to the 
+    "24Ours" S3 bucket and send the presigned (temporary) URL to the file back.
+    """
 
     # Send the body and name of the file to the s3 and get a response
     response = s3_client.put_object(
-        Body=b64_decode,
-        Key=event["fileName"],
+        # The body is the decoded event which is recieved in base 64
+        Body = b64decode(event["fileBody"]),
+        # The key is the fileName of the event
+        Key = event["fileName"],
+        # The S3 Bucket in which we store our files is named 24ours
         Bucket="24ours"
     )
     
     # Create a custom url on the fly that expires in 24 hours (the function takes seconds so its 86,400s)
     # The url does not link to the s3 since it is private but makes a temporary link to the s3
     get_url = s3_client.generate_presigned_url('get_object',
-                                                    Params = {
-                                                        'Bucket': "24ours",
-                                                        'Key': event["fileName"]
-                                                    },
-                                                    ExpiresIn = 86400)
+    # The bucket is named 24ours and the key to the item in the bucket is the name of the file, for example: bee.jpg
+        Params = {
+            'Bucket': "24ours",
+            'Key': event["fileName"]
+        },
+        ExpiresIn = 86400
+    )
     
-    # get = s3_client.get_object(
-    #     Bucket="24ours",
-    #     Key=event["fileName"],
-    #     )
-        
-    # print(get)
-    
+    # Return the default success status code and put url in the response body to be copied on the page
     return {
-        
         'statusCode': 200,
         'body': {
             "url": get_url
